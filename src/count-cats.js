6b8a7e5f7f6315d477c8c8b7775f8b5afb100cf8
onyxgeek <onyxpeek@gmail.com>
2023-04-13 19:02:50 +0300

    feat: add comments on the task

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,18 +14,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(matrix) {
-  let searchChar = '^^';
+function countCats(matrix) {//Here I used function declaration
+  let searchChar = '^^';//
   let count = 0;
   for(let i = 0; i < matrix.length; i++){
-      if(Array.isArray(matrix[i])){
-        count += countCats(matrix[i], searchChar);
+      if(Array.isArray(matrix[i])){//Here I checked if the there is more level of nested arrays
+        count += countCats(matrix[i], searchChar);//Here is a recursion with reclaing function by each time when there is array when not count++
       }
-      else if (matrix[i] === searchChar){
+      else if (matrix[i] === searchChar){//Here I checked if the current element is the '^^' then count++
         count++;
       }
   }
-  return count;
+  return count;//return the result
 }
 
 module.exports = {
