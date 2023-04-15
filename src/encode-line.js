cb7d21e34730e10bc6db9e563a35a61d27f0356d
onyxgeek <onyxpeek@gmail.com>
2023-04-15 11:06:28 +0300

    feat: add solution with comments to the encode-line.js

--- src/encode-line.js
+++ src/encode-line.js
@@ -19,11 +19,16 @@ function encodeLine(str) { // Here I declare the function
       count++;//Increment the value
     } else {
       result.push([count, str[i]]);//Here if it is not push value into result array
-      count = 1;//Here I do redeclare the value
+      count = 1;//Here I do redeclare the value by default
     }
   }
 
   return result.map((row) => row.join('')).join('').replace(/1/g, '');
+
+  //1 - map - going through each element of array because this is nested array and apply to each element which is a nested array method
+  //.join()
+  //2 - join('') turn the main array into string
+  //3 - replace() replace all the value of 1
 }
 
 
