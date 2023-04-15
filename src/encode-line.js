3bc44c7c8298672b96648bb4bbc22a51b9081332
onyxgeek <onyxpeek@gmail.com>
2023-04-15 11:02:31 +0300

    feat: add solution for the encode-line.js

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,10 +10,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) { // Here I declare the function
+  let count = 1; //Here I declare the counter of an char which is by default 1
+  let result = []; // here is the final array with result
+
+  for (let i = 0; i < str.length; i++) { //Here I do run the loop
+    if (str[i] === str[i + 1]) { //Check if the current element is the same as the next one
+      count++;//Increment the value
+    } else {
+      result.push([count, str[i]]);//Here if it is not push value into result array
+      count = 1;//Here I do redeclare the value
+    }
+  }
+
+  return result.map((row) => row.join('')).join('').replace(/1/g, '');
 }
+
+
+
+let a = 'aabbbc';
+console.log(encodeLine(a));
 
 module.exports = {
   encodeLine
